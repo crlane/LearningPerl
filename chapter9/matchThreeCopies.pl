9f9a8e4300d19fc851745373498fa3fe1465986c
Cameron Lane <lane.cameron@gmail.com>
2012-03-11 21:44:58 -0400

    basic outline of program

---
+++ chapter9/matchThreeCopies.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use strict;
+use 5.010;
+
+my $what = 'fred|barney';
+
+while (<>) {                   # take one input line at a time
+	chomp;
+	if (/(?$what){3}/) {
+		print "matched: |$`<$&>$'|\n";  # the special match vars
+	} else {
+		print "no match: |$_|\n";
+	}
+}
